cfffaff22222d4ec355b3ab7379231c0ee5a4535
neelimasajit <112232646+neelimasajit@users.noreply.github.com>
2022-08-27 17:27:56 +0530

    Searchbar (#8)
    * Added search bar
    
    * Added userhome path
    
    * Corrected format
    
    * Added payment popup
    
    Co-authored-by: neelima <neelimasajit@gmail.com>

--- src/pages/Enquiry/components/enquiryDetailCard.tsx
+++ src/pages/Enquiry/components/enquiryDetailCard.tsx
@@ -1,25 +1,36 @@
 import { Button } from 'components';
 import { ButtonType } from 'components/button/types';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import PaymentPopUp from '../../../components/paymentpopup/paymentpopup';
 
 const EnquiryDetailCard: FC = () => {
+  const [paymentModal, setPaymentModalOpen] = useState(false);
+
+  const cancelFn = () => {
+    setPaymentModalOpen(false);
+  };
+
   return (
-    <div className=' border-2 rounded p-4 shadow-md mb-4'>
-      <div className='flex justify-between'>
-        <div>Agency 1</div>
-        <Button type={ButtonType.BLUE} onClick={() => console.log('click')} className='items-center'>
-          View Offer
-        </Button>
+    <>
+      {paymentModal && <PaymentPopUp cancelFn={cancelFn} />}
+
+      <div className=' border-2 rounded p-4 shadow-md mb-4'>
+        <div className='flex justify-between'>
+          <div>Agency 1</div>
+          <Button type={ButtonType.BLUE} onClick={() => console.log('click')} className='items-center'>
+            View Offer
+          </Button>
+        </div>
+        <div className='flex'>
+          <Button type={ButtonType.GREEN} onClick={() => setPaymentModalOpen(true)} className='items-center mr-4'>
+            Accept
+          </Button>
+          <Button type={ButtonType.YELLOW} onClick={() => console.log('click')} className='items-center'>
+            Request a Change
+          </Button>
+        </div>
       </div>
-      <div className='flex'>
-        <Button type={ButtonType.GREEN} onClick={() => console.log('click')} className='items-center mr-4'>
-          Accept
-        </Button>
-        <Button type={ButtonType.YELLOW} onClick={() => console.log('click')} className='items-center'>
-          Request a Change
-        </Button>
-      </div>
-    </div>
+    </>
   );
 };
 
