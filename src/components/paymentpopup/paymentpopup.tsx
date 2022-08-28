67daf9f3244971bd867f05fb6771476a9f86978c
mhd-shibil <91175731+mhd-shibil@users.noreply.github.com>
2022-08-28 06:05:04 +0530

    User travel cards (#27)
    * Enquiry details Page
    
    * enquiry details page
    
    * Update enquiry card
    
    * Update Path
    
    * Mutation File
    
    * travelform Quoery
    
    * Api updates
    
    * API Updates
    
    * update form
    
    * update form
    
    * Update form submit
    
    * Update PDF Viewer

--- src/components/paymentpopup/paymentpopup.tsx
+++ src/components/paymentpopup/paymentpopup.tsx
@@ -1,5 +1,6 @@
-import { Button } from 'components';
+import CircularLoader from 'components/loader/CircularLoader';
 import React from 'react';
+import { toast } from 'react-toastify';
 //import { useState } from 'react';
 
 interface paymentDetails {
@@ -7,12 +8,18 @@ interface paymentDetails {
 }
 
 const PaymentPopUp: React.FC<paymentDetails> = ({ cancelFn }) => {
+  setTimeout(() => {
+    cancelFn();
+    toast.success('Payment Completed');
+  }, 3000);
+
   return (
     <div className='w-screen h-screen top-0 left-0 flex items-start flex-wrap bg-opacity-50 justify-center fixed  z-10'>
       <div className='bg-white w-96 p-10 my-auto text-center shadow-slate-300 rounded-md'>
-        <h2 className='font-bold'>Completed Payment!</h2> <br />
-        <p className='font-semibold'>Rs. 20000</p>
-        <div className='flex justify-center mt-5'>
+        <h2 className='font-bold'>Payment In Progress</h2> <br />
+        <CircularLoader fullScreen={false} />
+        {/* <p className='font-semibold'>Rs. 20000</p> */}
+        {/* <div className='flex justify-center mt-5'>
           <Button className='bg-green-500 w-24 mr-8 font-bold text-white' onClick={() => console.log('Paid')}>
             Pay
           </Button>
@@ -20,7 +27,7 @@ const PaymentPopUp: React.FC<paymentDetails> = ({ cancelFn }) => {
           <Button className='bg-red-400 w-24 font-bold text-white' onClick={cancelFn}>
             Cancel
           </Button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/pdfViewer/pdfViewer.jsx
+++ src/components/pdfViewer/pdfViewer.jsx
@@ -2,10 +2,9 @@ import { React, useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 import pdf from '../../assets/sample.pdf';
-import PaymentPopUp from '../paymentpopup/paymentpopup';
 // import { Modal } from '@mui/material';
 
-const PdfViewer = () => {
+const PdfViewer = ({ onClosePdf, onClickPayment }) => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
@@ -22,61 +21,66 @@ const PdfViewer = () => {
   function changePageForward() {
     changePage(1);
   }
-  const [paymentModal, setPaymentModalOpen] = useState(false);
-  const cancelFn = () => {
-    setPaymentModalOpen(false);
-  };
 
   return (
-    <>
-      {paymentModal && <PaymentPopUp cancelFn={cancelFn} />}
-      <div className=' p-4'>
+    <div className='w-screen h-screen bg-opacity-50 top-0 left-0 fixed'>
+      <div className=' p-4 mt-4'>
         <div className='flex w-full justify-center'>
           <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page height='600' pageNumber={pageNumber} />
+            <Page height='800' pageNumber={pageNumber} />
           </Document>
         </div>
-
-        <div className=''>
-          <p className=' p-2 rounded-md'>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
-        <div className=''>
-          {pageNumber > 1 && (
-            <button className=' p-2 rounded-md' onClick={changePageBack}>
-              Previous Page
-            </button>
-          )}
+        <div className='flex justify-center'>
+          <div className='flex  justify-center'>
+            <p className=' p-2 rounded-md'>
+              Page {pageNumber} of {numPages}
+            </p>
+          </div>
+          <div className='flex justify-center'>
+            {pageNumber > 1 && (
+              <button className=' p-2 rounded-md' onClick={changePageBack}>
+                Previous Page
+              </button>
+            )}
+          </div>
           {pageNumber < numPages && (
-            <button className=' p-2 rounded-md' onClick={changePageForward}>
+            <button className=' p-2 rounded-md ' onClick={changePageForward}>
               Next Page
             </button>
           )}
         </div>
-        <div className='p-2'>
-          <button
-            className=' p-2 rounded-md text-white'
-            style={{ backgroundColor: 'green' }}
-            onClick={() => setPaymentModalOpen(true)}
-          >
-            Accept
-          </button>
-        </div>
-        <div className='p-2'>
-          <button className=' p-2 rounded-md  text-white' style={{ backgroundColor: 'red' }} onClick={''}>
-            Not Interested
-          </button>
-        </div>
-        <div className='p-2'>
-          <button className=' p-2 rounded-md  ' style={{ backgroundColor: 'lightsteelblue' }}>
-            <a href='http://www.africau.edu/images/default/sample.pdf' target='_blank' rel='noreferrer'>
-              Download
-            </a>
-          </button>
+        <div className='flex justify-center'>
+          <div className='p-2 flex'>
+            <button
+              className=' p-2 rounded-md text-white'
+              style={{ backgroundColor: 'green' }}
+              onClick={() => onClickPayment()}
+            >
+              Accept
+            </button>
+          </div>
+          <div className='p-2'>
+            <button
+              className=' p-2 rounded-md  text-white'
+              style={{ backgroundColor: 'red' }}
+              onClick={() => {
+                console.log(99);
+                onClosePdf();
+              }}
+            >
+              Not Interested
+            </button>
+          </div>
+          <div className='p-2'>
+            <button className=' p-2 rounded-md  ' style={{ backgroundColor: 'lightsteelblue' }}>
+              <a href='http://www.africau.edu/images/default/sample.pdf' target='_blank' rel='noreferrer'>
+                Download
+              </a>
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
